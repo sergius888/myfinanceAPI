9bcdc6aef63cd28a24ffaf344f77447065667833
Sergiu <sergiu.chiricescu@icloud.com>
2022-05-21 17:05:55 +0300

    fix start error

--- project_1/my_finance/index.py
+++ project_1/my_finance/index.py
@@ -26,7 +26,7 @@ from my_finance.api.diagrams import diagrams_router
 app = FastAPI(
     title="Name of our app",  # TODO for homework, name your application
     # <major_version>.<minor_version>.<patch_version>
-    version="0.2.0",  # increase version after finishing homework
+    version="1.0.0",  # increase version after finishing homework
     description="",  # TODO add a description
 )
 app.include_router(stocks_router)
